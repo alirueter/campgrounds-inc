222a553cbc7a510b036afe7b82fd3e1631685231
Alexsis Conley Brown <alexsismcb@gmail.com>
2020-09-08 19:25:32 -0500

    user.js update

--- models/User.js
+++ models/User.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-=======
-//const { Model } = require("sequelize");
-
->>>>>>> 42ff6e76ba6e93ddc87c395fe6420135eef19421
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 
