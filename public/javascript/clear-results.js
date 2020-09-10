6559f4d9835bed4b6b1aae9377e8b327f629f10a
KWedwick <64446581+kwedwick@users.noreply.github.com>
2020-09-10 18:35:48 -0500

    changed clear results id query

--- public/javascript/clear-results.js
+++ public/javascript/clear-results.js
@@ -1,5 +1,5 @@
 function clearResults(){
-    document.getElementById('search-results').innerHTML = "";
+    document.getElementById('search-results-wrapper').innerHTML = "";
 }
 
 document.querySelector('#clear-button').addEventListener('click', function(event) {
