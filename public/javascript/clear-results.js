348c2ae35c25bacd2849d1d67b52276e4f2ee6b1
alirueter <rueter.alison@gmail.com>
2020-09-10 10:22:40 -0500

    clear results working

--- public/javascript/clear-results.js
+++ public/javascript/clear-results.js
@@ -1,6 +1,13 @@
+console.log("clear-results.js is loaded");
+
 function clearResults(){
+    console.log("clearResults running....");
+
     // document.querySelector('.search-results').remove();
     document.getElementById('search-results').innerHTML = "";
 }
 
-document.querySelector('#clear-button').addEventListener('submit', clearResults);
+document.querySelector('#clear-button').addEventListener('click', function(event) {
+    event.preventDefault();
+    clearResults();
+    });
