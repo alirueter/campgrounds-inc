4ea5404fc1def110a9028e1f39f515f77ee8ce05
Alexsis Conley Brown <alexsismcb@gmail.com>
2020-09-10 06:24:41 -0500

    search generator

--- public/javascript/state-search.js
+++ public/javascript/state-search.js
@@ -1,3 +1,4 @@
+// fetch national park api
 var campgroundStateSearch = function(event) {
     event.preventDefault();
 
@@ -9,42 +10,83 @@ var campgroundStateSearch = function(event) {
             return response.json();
         }).then(function(data) {
             const cg = data.data;
-            
-            for (var i = 0; i < cg.length; i++) {
 
-                //saveCampground();
-                console.log(cg[i]);
+            generateSearch(cg);
 
-                // these were added to reference back to later 
-
-                /*const phoneNumber = cg[i].contacts.phoneNumbers;
-                const emailAddress = cg[i].contacts.emailAddresses;
-                const cost = cg[i].fees;
-                const costDescription = cg[i].fees;
-                const address = cg[i].addresses;
-                
-                
-                console.log(cg[i].name,",", 
-                    cg[i].id, ",",
-                    cg[i].description, ",",
-                    cg[i].reservationInfo, ",",
-                    phoneNumber[i].phoneNumber, ",",
-                    emailAddress[i].emailAddress, ",",
-                    cost[i].cost, ",",
-                    costDescription[i].description, ",",
-                    address[i].line1, ",",
-                    address[i].city, ",",
-                    address[i].stateCode, ",",
-                    address[i].postalCode);*/
-                
-            }
+            //saveCampground();                
         });
-}
+};
 
-// add this function to a function that gets the response from select dropdown
-// can see the call in action in console log if replace state with a string of stateCode
-// example campgroundStateSearch('WI');
-//campgroundStateSearch(state);
+function generateSearch(campgrounds) {
+
+    const campgroundsArray = campgrounds;
+
+    for (var i = 0; i < campgroundsArray.length; i++) {
+
+        const searchResultsList = document.querySelector('#search-results');
+        const address = campgroundsArray[i].addresses[0];
+        const email = campgroundsArray[i].contacts.emailAddresses[0];
+        const number = campgroundsArray[i].contacts.phoneNumbers[0];
+        const picture = campgroundsArray[i].images[0];
+
+        // append camp name
+        var campName = document.createElement("li");
+        campName.innerHTML = '<h6>' + campgroundsArray[i].name + '</h6>';
+        searchResultsList.appendChild(campName);
+
+        console.log(campgroundsArray[i].images[0]);
+
+        // append image
+        if (picture == null || picture.url == "") {
+            console.log('No picture!');
+        } else {
+           var campImage = document.createElement("p");
+           campImage.innerHTML = '<img src="' + picture.url +'" credit="'
+            + picture.credit + '" alt="'
+            + picture.altText + '">';
+            
+            searchResultsList.appendChild(campImage)
+        }
+
+        // append address
+        if (address == null || address.line1 == "") {
+            var noAddress = document.createElement("li");
+            noAddress.innerHTML = "No address available";
+            searchResultsList.appendChild(noAddress);
+        } else {
+            var validAddress = document.createElement("li");
+
+            validAddress.innerHTML = '<p>• ' + address.line1 +
+            '</p><p>• ' + address.city + ', ' + address.stateCode + ', '
+            + address.postalCode + '</p>';
+            searchResultsList.appendChild(validAddress);
+        }
+
+        // append number
+        if (number == null || number.phoneNumber == "") {
+            var noNumber = document.createElement("li");
+            noNumber.innerHTML = "<b>Number: N/A</b>";
+            searchResultsList.appendChild(noNumber);
+        } else {
+            var validNumber = document.createElement("li");
+
+            validNumber.innerHTML = '<b>Number:</b> ' + number.phoneNumber;
+            searchResultsList.appendChild(validNumber);
+        }
+
+        // append email
+        if (email == null || email.emailAddress == "") {
+            var noEmail = document.createElement("li");
+            noEmail.innerHTML = "<b>Email: N/A</b>";
+            searchResultsList.appendChild(noEmail);
+        } else {
+            var validEmail = document.createElement("li");
+
+            validEmail.innerHTML = '<b>Email:</b> ' + email.emailAddress + '</br></br>';
+            searchResultsList.appendChild(validEmail);
+        }
+    }
+};
 
 async function saveCampground(event) {
     event.preventDefault();
@@ -74,4 +116,4 @@ async function saveCampground(event) {
 
 //document.querySelector('.campground-list-item').addEventListener('submit', saveCampground);
 
-document.querySelector("#cg-state-search-form").addEventListener('submit', campgroundStateSearch)
+document.querySelector("#cg-state-search-form").addEventListener('submit', campgroundStateSearch);
